fdf849cccff27c3b421375e37c52801bbc773bae
Phạm Thị Mai Linh <124490245+Linhpham24-lp@users.noreply.github.com>
2023-02-08 14:14:00 +0700

    Update 06-Filtering and finishing touches.sql

--- 06-Filtering and finishing touches.sql
+++ 06-Filtering and finishing touches.sql
@@ -26,3 +26,73 @@ WHERE athlete_id IN
      FROM athletes
      WHERE age <= 16);
 
+
+--Report 2: Top athletes in nobel-prized countries
+-- Pull event and unique athletes from summer_games 
+SELECT 	
+	event,
+	COUNT(athlete_id) AS athletes
+FROM summer_games
+GROUP BY 1;
+
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+	event, 
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female'
+    ELSE  'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+GROUP BY event;
+
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id
+    FROM country_stats
+    WHERE nobel_prize_winners > 0)
+GROUP BY event;
+
+
+
+-- Pull event and unique athletes from summer_games 
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id 
+    FROM country_stats 
+    WHERE nobel_prize_winners > 0)
+GROUP BY event
+-- Add the second query below and combine with a UNION
+UNION
+SELECT 
+    event,
+    -- Add the gender field below
+    CASE WHEN event LIKE '%Women%' THEN 'female' 
+    ELSE 'male' END AS gender,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM winter_games
+-- Only include countries that won a nobel prize
+WHERE country_id IN 
+	(SELECT country_id 
+    FROM country_stats 
+    WHERE nobel_prize_winners > 0)
+GROUP BY event
+-- Order and limit the final output
+ORDER BY athletes DESC
+LIMIT 10;
